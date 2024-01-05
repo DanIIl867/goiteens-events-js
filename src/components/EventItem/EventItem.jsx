341806ccba71595761220a583dca984cacad07cb
Daniil <negativdaniil4@gmail.com>
2024-01-05 21:34:24 +0200

    Update EventItem.jsx

--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -11,13 +11,18 @@ export const EventItem = ({
 	time,
 	iconCalendar: CiCalendar,
 	iconPerson: FaPerson, }) => {
+
+	const firstDate = Date.parse(time.start)
+	const secondDate = Date.parse(time.end)
+	const fullTime = (secondDate - firstDate) / 3600000
+
 	return (
 		<ListItem>
 			<ListTitle>{name}</ListTitle>
 			<ListText> <CiLocationOn />{location}</ListText>
 			<ListText> <FaPerson />{speaker}</ListText>
 			<ListText><CiCalendar />{type}</ListText>
-			<ListText><CiAlarmOn />time</ListText>
+			<ListText><CiAlarmOn />{fullTime} time</ListText>
 		</ListItem>
 	)
 }
